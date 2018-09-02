9f3daec77d1f8eca6a5b975f6d0f59c18fbb790a
Ilya Siz <ilya.siz@preservica.com>
2018-09-02 13:41:10 +0300

    Axios support added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -12,6 +13,8 @@ const PARAM_HPP = 'hitsPerPage=';
 
 class App extends Component {
 
+    _isMounted = false;
+
     constructor(props) {
         super(props);
         this.state = {
@@ -23,10 +26,9 @@ class App extends Component {
     }
 
     fetchSearchTopStories = (searchTerm, page = 0) => {
-        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-            .then(response => response.json())
-            .then(result => this.setSearchTopStories(result))
-            .catch(error => this.setState({ error }));
+        axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+            .then(result => this._isMounted && this.setSearchTopStories(result.data))
+            .catch(error => this._isMounted && this.setState({ error }));
     };
 
     setSearchTopStories = result => {
@@ -48,10 +50,15 @@ class App extends Component {
     };
 
     componentDidMount = () => {
+        this._isMounted = true;
         const { searchTerm } = this.state;
         this.setState({ searchKey: searchTerm });
         this.fetchSearchTopStories(searchTerm);
     };
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
 
     onSearchChange = (event) => {
         this.setState({ searchTerm: event.target.value });
