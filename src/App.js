658bce268b28c90ddfcadb0541f603ba1d6dad61
Ilya Siz <ilya.siz@preservica.com>
2018-09-02 15:10:32 +0300

    PropTypes added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -135,6 +136,7 @@ class App extends Component {
     }
 }
 
+
 const Search = ({
     value,
     onChange,
@@ -150,6 +152,14 @@ const Search = ({
             {children}
         </button>
     </form>;
+
+Search.propTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    children: PropTypes.string
+};
+
 
 const Table = ({
     list,
@@ -175,9 +185,23 @@ const Table = ({
         )}
     </div>;
 
+Table.propTypes = {
+    list: PropTypes.arrayOf(
+        PropTypes.shape({
+            objectID: PropTypes.string.isRequired,
+            author: PropTypes.string,
+            url: PropTypes.string,
+            num_comments: PropTypes.number,
+            points: PropTypes.number,
+        })
+    ).isRequired,
+    onDismiss: PropTypes.func.isRequired,
+};
+
+
 const Button = ({
     onClick,
-    className = '',
+    className,
     children,
 }) =>
     <button
@@ -186,6 +210,17 @@ const Button = ({
         type="button">
         {children}
     </button>;
+
+Button.defaultProps = {
+    className: '',
+};
+
+Button.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    className: PropTypes.string,
+    children: PropTypes.node.isRequired,
+};
+
 
 export default App;
 
